02fc67eaceb346b0c27f594a87d08960cd3ae365
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2024-11-10 22:10:07 +0100

    date type

--- dev/init-db.sql
+++ dev/init-db.sql
@@ -2,11 +2,11 @@ create table bookmarks (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     url text NOT NULL,
     title text NOT NULL,
-    creationDate bigint NOT NULL
+    creationDate DATE NOT NULL
 );
 
 create table tags (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     label varchar(100) NOT NULL,
-    creationDate bigint NOT NULL
+    creationDate DATE NOT NULL
 );
