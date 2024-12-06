4d10292742f625bd27b3aa52278e5ac601737a37
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2024-12-06 20:09:12 +0100

    better error reporting

--- model/add.go
+++ model/add.go
@@ -2,7 +2,10 @@ package model
 
 import (
 	"context"
+	"fmt"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 func AddBookmark(request BookmarkCreationRequest) (*Bookmark, error) {
@@ -18,6 +21,9 @@ func AddBookmark(request BookmarkCreationRequest) (*Bookmark, error) {
 	var creationDate time.Time
 	dbInsertErr := conn.QueryRow(context.TODO(), sqlInsertQuery, request.Url, request.Title).Scan(&id, &creationDate)
 	if dbInsertErr != nil {
+		if pgErr, ok := dbInsertErr.(*pgconn.PgError); ok {
+			return nil, handleDatabaseError(pgErr)
+		}
 		return nil, dbInsertErr
 	}
 
@@ -31,14 +37,21 @@ func AddTag(request TagCreationRequest) (*Tag, error) {
 	}
 	defer conn.Close(context.TODO())
 
-	sqlInsertQuery := "insert into tags (label, creationDate) values ($1, now()) returning (id, creationDate)"
+	sqlInsertQuery := "insert into tags (label, creationDate) values ($1, now()) returning id, creationDate"
 
 	var id uint64
 	var creationDate time.Time
 	dbInsertErr := conn.QueryRow(context.TODO(), sqlInsertQuery, request.Label).Scan(&id, &creationDate)
 	if dbInsertErr != nil {
+		if pgErr, ok := dbInsertErr.(*pgconn.PgError); ok {
+			return nil, handleDatabaseError(pgErr)
+		}
 		return nil, dbInsertErr
 	}
 
 	return &Tag{Label: request.Label, CreationDate: creationDate, Id: id}, nil
+}
+
+func handleDatabaseError(pgErr *pgconn.PgError) error {
+	return fmt.Errorf("DB error occurred, code: %s, message: '%s', details: '%s'", pgErr.Code, pgErr.Message, pgErr.Detail)
 }
